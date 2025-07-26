8c1a4133976de280a11204a1eebfdfd39872cc19
ĐÀO VĨNH KHANG <khangdao0311@gmail.com>
2025-07-26 13:29:52 +0700

    google-site-verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,29 @@ import { ToastContainer } from "react-toastify";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Portfolio - Khangdao0311",
-  description: "information for me",
+  title: "Khang Đào | Front-End Developer",
+  description:
+    "Portfolio cá nhân của Khang Đào - Front-End Developer chuyên ReactJS, Next.js, TailwindCSS...",
+  keywords: [
+    "Khang Đào",
+    "Front-End Developer",
+    "Next.js",
+    "Portfolio",
+    "khangdao311",
+    "ReactJS",
+  ],
+  metadataBase: new URL("https://khangdao0311-portfolio.vercel.app"),
+  openGraph: {
+    title: "Khang Đào | Front-End Developer",
+    description:
+      "Xem các dự án cá nhân và kỹ năng của Khang Đào - Lập trình viên giao diện hiện đại.",
+    url: "https://khangdao0311-portfolio.vercel.app",
+    siteName: "Khang Đào Portfolio",
+    type: "website",
+  },
+  other: {
+    "google-site-verification": "k9plqj4p_Epg55DIWsjNnsh0sze1tnOnYQq90SQUg18",
+  },
 };
 
 export default function RootLayout({
